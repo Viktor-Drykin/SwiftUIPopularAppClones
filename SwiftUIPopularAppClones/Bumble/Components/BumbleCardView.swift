d3583b377df1de0353ae85b8d2169ef9d357a58d
Viktor-Drykin <142505425+Viktor-Drykin@users.noreply.github.com>
2024-09-20 15:52:35 +0100

    add bumble filter view and navigation bar

---
+++ SwiftUIPopularAppClones/Bumble/Components/BumbleCardView.swift
@@ -0,0 +1,18 @@
+//
+//  BumbleCardView.swift
+//  SwiftUIPopularAppClones
+//
+//  Created by Viktor Drykin on 20.09.2024.
+//
+
+import SwiftUI
+
+struct BumbleCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BumbleCardView()
+}
---
+++ SwiftUIPopularAppClones/Bumble/Components/BumbleFilterView.swift
@@ -0,0 +1,55 @@
+//
+//  BumbleFilterView.swift
+//  SwiftUIPopularAppClones
+//
+//  Created by Viktor Drykin on 19.09.2024.
+//
+
+import SwiftUI
+
+struct BumbleFilterView: View {
+
+    var options: [String] = ["Everyone", "Tranding"]
+    @Binding var selection: String
+    @Namespace private var namespace
+
+    var body: some View {
+        HStack(alignment: .top, spacing: 32) {
+            ForEach(options, id: \.self) { option in
+                VStack(spacing: 8) {
+                    Text(option)
+                        .frame(maxWidth: .infinity)
+                        .font(.subheadline)
+                        .fontWeight(.medium)
+
+                    if selection == option {
+                        RoundedRectangle(cornerRadius: 2)
+                            .frame(height: 1.5)
+                            .matchedGeometryEffect(id: "selection", in: namespace)
+                    }
+                }
+                .padding(.top, 8)
+                .background(Color.black.opacity(0.001))
+                .foregroundStyle(selection == option ? .bumbleBlack : .bumbleGray)
+                .onTapGesture {
+                    selection = option
+                }
+            }
+        }
+        .animation(.smooth, value: selection)
+    }
+}
+
+fileprivate struct BumbleFilterViewPreview: View {
+    var options = ["Everyone", "Tranding"]
+    @State private var selection = "Everyone"
+
+    var body: some View {
+        BumbleFilterView(options: options, selection: $selection)
+    }
+}
+
+#Preview {
+    BumbleFilterViewPreview()
+        .padding()
+}
